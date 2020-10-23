f807eb8cab49582421758987961e0c352483b3a1
Yory <746972747@qq.com>
2020-10-23 22:38:37 +0800

    fix user login logic

--- src/main/java/com/yoryz/file/share/common/advanced/CookieUtils.java
+++ src/main/java/com/yoryz/file/share/common/advanced/CookieUtils.java
@@ -206,20 +206,10 @@ public class CookieUtils {
             serverName = serverName.substring(7);
             final int end = serverName.indexOf("/");
             serverName = serverName.substring(0, end);
-            final String[] domains = serverName.split("\\.");
-            int len = domains.length;
-            if (len > 3) {
-                // www.xxx.com.cn
-                domainName = "." + domains[len - 3] + "." + domains[len - 2] + "." + domains[len - 1];
-            } else if (len <= 3 && len > 1) {
-                // xxx.com or xxx.cn
-                domainName = "." + domains[len - 2] + "." + domains[len - 1];
-            } else {
-                domainName = serverName;
-            }
+            domainName = serverName;
         }
 
-        if (domainName != null && domainName.indexOf(":") > 0) {
+        if (domainName.indexOf(":") > 0) {
             String[] ary = domainName.split("\\:");
             domainName = ary[0];
         }
